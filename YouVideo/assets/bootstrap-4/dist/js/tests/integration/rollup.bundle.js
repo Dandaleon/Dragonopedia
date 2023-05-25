c867a505d69357a8e9f35539daba32f894e53a45
Dandaleon <85185237+Dandaleon@users.noreply.github.com>
2023-05-25 12:35:46 +0300

    Add files via upload

---
+++ YouVideo/assets/bootstrap-4/dist/js/tests/integration/rollup.bundle.js
@@ -0,0 +1,18 @@
+const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require("rollup-plugin-commonjs");
+const babel = require("rollup-plugin-babel");
+
+module.exports = {
+  input: "js/tests/integration/bundle.js",
+  output: {
+    file: "js/coverage/bundle.js",
+    format: "iife",
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+  ],
+};
