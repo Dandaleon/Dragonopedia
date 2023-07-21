83b906a45aeb88a16960c6a813e4f6bad5bae375
Dandaleon <workenplace@gmail.com>
2023-07-21 16:52:13 +0300

    Add New section catalog

--- RunSmart/js/script.js
+++ RunSmart/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
   $(".carousel__inner").slick({
     speed: 1200,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 5000,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/chevron-left-solid.png"></img></button>',
-    nextArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/chevron-right-solid.png"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/chevron-right-solid.png"></img></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
   });
 });
