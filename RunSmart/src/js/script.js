66f1671bdcb5b4c67d134cdaeafb98bfce5991bb
Dandaleon <workenplace@gmail.com>
2023-07-26 16:00:01 +0300

    Finish footer

--- RunSmart/src/js/script.js
+++ RunSmart/src/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     // adaptiveHeight: true,
     autoplay: true,
     autoplaySpeed: 5000,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/chevron-left-solid.png"></img></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/chevron-right-solid.png"></img></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/chevron-left-solid.png"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/chevron-right-solid.png"></img></button>',
     responsive: [
       {
         breakpoint: 992,
