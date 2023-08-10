13616c5ded93afe09fea5b0c3020a73282bf24c1
Dandaleon <workenplace@gmail.com>
2023-08-10 16:38:10 +0300

    Adapt 1/2 scrin

--- Portfolio/dist/js/script.js
+++ Portfolio/dist/js/script.js
@@ -16,3 +16,46 @@ const counter = document.querySelectorAll(".skills__counters-percent"),
 counter.forEach((item, i) => {
   line[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function () {
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        email: {
+          required: true,
+          email: true,
+        },
+        textarea: {
+          required: true,
+          minlength: 20,
+        },
+        agree: {
+          required: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Будь ласка, введіть своє ім'я",
+          minlength: jQuery.validator.format("Введіть {0} символи!"),
+        },
+        email: {
+          required: "Будь ласка, введіть свою пошту",
+          email: "Неправильно введена адреса пошти",
+        },
+        textarea: {
+          required: "Будь ласка, введіть ваше повідомлення",
+          minlength: jQuery.validator.format("Введіть {0} символів!"),
+        },
+        agree: {
+          required: "Будь ласка, ознайомтесь та підтвердіть, що ви згодні з нашою політикою конфеденційності",
+        },
+      },
+    });
+  }
+
+  validateForms("#contacts-form");
+});
