1ef1d323d749a5aa8900dfdd0428eee50b2c884a
Dandaleon <workenplace@gmail.com>
2023-08-28 16:38:28 +0300

    Write js.script for menu

--- game/dist/js/script.js
+++ game/dist/js/script.js
@@ -1,7 +1,24 @@
-const upgrade = document.getElementsByClassName(".company__btn-upgrade");
+const upgrade = document.querySelector(".company__btn-upgrade"),
+  grade = document.querySelector(".grade"),
+  closeUp = document.querySelector(".grade__btn-close"),
+  pickAccaunt = document.querySelectorAll(".players-list__item");
 
-console.log(upgrade);
+if (upgrade) {
+  upgrade.addEventListener("click", () => {
+    grade.classList.add("grade__active");
+  });
+}
 
-upgrade.addEventListener("click", () => {
-  grade.classList.add("active");
-});
+if (closeUp) {
+  closeUp.addEventListener("click", () => {
+    grade.classList.remove("grade__active");
+  });
+}
+
+if (pickAccaunt) {
+  pickAccaunt.forEach((item) =>
+    item.addEventListener("click", () => {
+      grade.classList.remove("grade__active");
+    })
+  );
+}
