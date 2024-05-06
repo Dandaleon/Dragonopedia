ae2047fc56d9d07d1452f8d6520e3fe94e8d742f
Dragonlinger <Arxangel1234567890@gmail.com>
2024-05-06 22:37:02 +0300

    refacting

---
+++ src/js/minigames.js
@@ -0,0 +1,9 @@
+const img = document.createElement('img');
+let color = ['Красный', 'Оранжевый', 'Жёлтый', 'Зелёный', 'Голубой', 'Синий', 'Фиолетовый']
+
+let minigames = {
+   quest: 'Какого цвета дракон на изображении?',
+   image: img.src = '../img/game.jpg',
+   answer: color
+};
+
