cff2b890d366e26829460bb83af7f81c7e393501
Dragonlinger <Arxangel1234567890@gmail.com>
2024-05-22 22:19:04 +0300

    Dandaleon upgrage site

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,45 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let pauseAnimateElement = document.querySelector(".dandaleon");
-  function resetButton() {
-    pauseAnimateElement.classList.remove("dandaleon__pause");
-  }
+  document.querySelector(".dandaleon").addEventListener("click", (e) => {
+    e.preventDefault();
+    let page = document.querySelector(".page");
+    page.classList.add("page-scroll");
+    document.querySelector(".main-content").style.cssText = "overflow: unset";
+    document.querySelector(".bg").classList.add("bg-scroll");
 
-  pauseAnimateElement.addEventListener("mouseover", () => {
-    pauseAnimateElement.classList.add("dandaleon__pause");
-
-    setTimeout(resetButton, 3000);
+    setTimeout(() => {
+      window.location.replace("dandaleon.html");
+    }, 1000);
   });
 });
 
 const btnUp = {
-  el: document.querySelector('.btn-up'),
+  el: document.querySelector(".btn-up"),
   show() {
     // удалим у кнопки класс btn-up_hide
-    this.el.classList.remove('btn-up_hide');
+    this.el.classList.remove("btn-up_hide");
   },
   hide() {
     // добавим к кнопке класс btn-up_hide
-    this.el.classList.add('btn-up_hide');
+    this.el.classList.add("btn-up_hide");
   },
   addEventListener() {
     // при прокрутке содержимого страницы
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       // определяем величину прокрутки
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
       scrollY > 400 ? this.show() : this.hide();
     });
     // при нажатии на кнопку .btn-up
-    document.querySelector('.btn-up').onclick = () => {
+    document.querySelector(".btn-up").onclick = () => {
       // переместим в начало страницы
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
-    }
-  }
-}
+    };
+  },
+};
 
 btnUp.addEventListener();
