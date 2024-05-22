aa6d14b5adafc8358035d4382b7be69909f632ee
Dandaleon <workenplace@gmail.com>
2024-05-22 22:55:12 +0300

    fix bag

--- dist/js/minigames.js
+++
@@ -1,9 +0,0 @@
-let quest = ['Какого цвета дракон на изображении?', 'Это цветной или металлический дракон?'];
-const img = document.createElement('img');
-let color = ['Красный', 'Оранжевый', 'Жёлтый', 'Зелёный', 'Голубой', 'Синий', 'Фиолетовый'];
-
-let minigames = {
-   quest: quest,
-   image: img.src = '../img/game.jpg',
-   answer: color
-};
